cf18f61a1d8abba176aa5ee5993e5900484c3ba8
System Administrator <root@Shanes-MacBook-Pro.local>
2024-01-23 22:35:34 -0600

    issue with php

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <JsonUploader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JsonUploader from './components/json-uploader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JsonUploader
   }
 }
 </script>
